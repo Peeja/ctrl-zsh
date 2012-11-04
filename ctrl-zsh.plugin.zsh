2c95d808dd06b02f5cf7aa0eb96fbc26c3de2c06
Peter Jaros <peter.a.jaros@gmail.com>
2012-11-04 16:29:09 -0500

    Explicitly address emacs keybinding.

--- ctrl-zsh.plugin.zsh
+++ ctrl-zsh.plugin.zsh
@@ -1,6 +1,6 @@
 # ^Z to foreground the last suspended job.
 foreground-current-job() { fg; }
 zle -N foreground-current-job
-bindkey '^z' foreground-current-job
+bindkey -M emacs '^z' foreground-current-job
 bindkey -M viins '^z' foreground-current-job
 bindkey -M vicmd '^z' foreground-current-job
